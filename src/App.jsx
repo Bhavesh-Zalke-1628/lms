7912624020ad3af9450922e3d76acda7e58edda0
Bhavesh Zalke <zalkebhavesh@gmail.com>
2024-01-04 11:23:15 +0530

    Add the eslint plugin auto import sort

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,11 @@
 import React from 'react'
-
+import { Route, Routes } from 'react-router-dom'
 const App = () => {
   return (
     <>
+    <Routes>
+      <Route path=''></Route>
+    </Routes>
       <h1 className='text-5xl text-yellow-400'>hello</h1>
     </>
   )
